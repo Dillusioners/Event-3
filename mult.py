004c2db8707bd2add4cc0728dea67aa47fdd2517
Itz OP <106766934+ItzOP69@users.noreply.github.com>
2022-06-14 12:04:13 +0530

    Add files via upload

---
+++ mult.py
@@ -0,0 +1,7 @@
+a = int(input("Enter a number: ")) # taking input from user and storing in 'a'
+c = a
+i = 1 # loop and multiplication purposes
+while i <= 10:
+    z = c * i # z stores the total value of the multiplication, i.e. 69 * 1 = 69 <--- z
+    print(c, "x", i, "=", z)
+    i += 1
